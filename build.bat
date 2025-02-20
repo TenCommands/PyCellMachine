7f5ad18c40437d9f1bdf6bcfd95fa5fb66c49bf3
TenCommands <tencommands1560@gmail.com>
2025-02-19 20:06:59 -0800

    still trying to figure out building

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 cd pycellmachine
 python -m \python\Scripts\activate
-pyinstaller --onefile __main__.py --add-data="settings.json:." --add-data="mods/:." --add-data="texturepacks/:." --add-data="_internal/:." --hidden-import win32api
+pyinstaller --onefile --noconsole __main__.py
